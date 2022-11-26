02591fc8a6e75e93eee9a7ebb0945397449ab99a
Niko <nicolo.donvito01@gmail.com>
2022-11-26 23:51:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+// Greet the user
+var date = new Date();
+var currentTime = date.getHours();
+if (currentTime > 4 && currentTime <= 12) {
+  document.getElementById("greetings").innerHTML = "Good morning! <br> I am Nicolò Donvito,"
+} else if (currentTime > 12 && currentTime <= 18) {
+  document.getElementById("greetings").innerHTML = "Good afternoon! <br> I am Nicolò Donvito,"
+} else if (currentTime > 18 && currentTime <= 24) {
+  document.getElementById("greetings").innerHTML = "Good evening! <br> I am Nicolò Donvito,"
+} else {
+  document.getElementById("greetings").innerHTML = "Welcome! <br> I am Nicolò Donvito,"
+}
+
+// Auto typer
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["Junior Game designer 🎮", "Tech Enthusiast 🤖", "Web Developer 💻"];
+const typingDelay = 20;
+const erasingDelay = 30;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  }
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay); 
+  }
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () { // On DOM Load initiate the effect
+  if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+// Nav selector
+const sections = document.querySelectorAll("section[id]");
+document.querySelector(".nav-sections a[href*=home]").classList.add("active-link");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav-sections a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav-sections a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+
+
+// Keep scroll position
+$(window).scroll(function () {
+  sessionStorage.scrollTop = $(this).scrollTop();
+});
+
+$(document).ready(function () {
+  if (sessionStorage.scrollTop != "undefined") {
+    $(window).scrollTop(sessionStorage.scrollTop);
+  }
+});
